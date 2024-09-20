14c58319e37c69fd3004033d006d506c6c8fb4ac
saa1986 <1986zip@mail.ru>
2024-09-20 11:42:03 +0500

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -1,36 +1,102 @@
+"""
+В классе House создайте атрибут houses_history = [], который будет хранить названия созданных объектов.
+
+Правильней вписывать здание в историю сразу при создании объекта, тем более можно удобно обращаться к
+атрибутам класса используя ссылку на сам класс - cls.
+Дополните метод __new__ так, чтобы:
+Название объекта добавлялось в список cls.houses_history.
+Название строения можно взять из args по индексу.
+
+Также переопределите метод __del__(self) в котором будет выводиться строка:
+"<название> снесён, но он останется в истории"
+
+Создайте несколько объектов класса House и проверьте работу методов __del__ и __new__,
+а также значение атрибута houses_history.
+"""
+
+
 class House:
-    houses_history = []  # Атрибут класса для хранения названий домов
+    """
+    Создает экземпляр дома
+    """
+    name: str
+    number_of_floors: int
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        House.houses_history.append(self.name)  # Добавляем название дома в историю
 
-    @classmethod
-    def new(cls, *args):
-        # Создание нового объекта House и добавление его имени в историю
-        if len(args) < 2:
-            raise ValueError("Необходимо указать название и количество этажей.")
-        name = args[0]
-        number_of_floors = args[1]
-        return cls(name, number_of_floors)
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i, end=' ')
+        print('\n')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
 
     def __del__(self):
         print(f"{self.name} снесён, но он останется в истории")
 
-# Пример использования
-h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)  # ['ЖК Эльбрус']
+h1 = House('Изенгард', 40)
+h2 = House('Ривенделл', 50)
+h3 = House('ЖК Бильбо Сумкин', 20)
 
-h2 = House.new('ЖК Акация', 20)
-print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация']
+print(House.houses_history)
 
-h3 = House.new('ЖК Матрёшки', 20)
-print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
+del h1
+del h3
 
-# Удаление объектов
-del h2  # ЖК Акация снесён, но он останется в истории
-del h3  # ЖК Матрёшки снесён, но он останется в истории
-
-print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
-del h1  # ЖК Эльбрус снесён, но он останется в истории
+print(House.houses_history)
